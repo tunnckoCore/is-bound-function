c0a5b1b077070b370700a9e04316f5f276a15168
tunnckoCore <mameto_100@mail.bg>
2016-04-12 21:09:56 +0300

    clarify test that use `bind-context` lib

--- test.js
+++ test.js
@@ -12,6 +12,21 @@
 var test = require('assertit')
 var isBoundFunction = require('./index')
 var bindContext = require('bind-context')
+var cleanupCode = require('cleanup-coverage-code')
+
+/**
+ * Tweak for coverage process. It minifies the code
+ * and adds semicolons.
+ *
+ * @param  {String} `val`
+ * @return {String}
+ */
+function cleanup (val) {
+  val = cleanupCode(val)
+  val = val.replace(';', '')
+  val = val.replace('\)\{', ') {')
+  return val
+}
 
 test('should return `true` when function is bound with native `.bind` function', function (done) {
   /* istanbul ignore next */
@@ -25,10 +40,12 @@ test('should return `true` when function is bound with native `.bind` function',
 
 test('should return `false` when function is bound with `bind-context` lib', function (done) {
   /* istanbul ignore next */
-  function fixture () {}
+  function fixture () { return true }
   var boundFn = bindContext({foo: 'bar'}, fixture)
 
   test.strictEqual(isBoundFunction(boundFn), false)
   test.strictEqual(isBoundFunction(fixture), false)
+  test.strictEqual(cleanup(boundFn.toString()), 'function fixture() {return true}')
+  test.strictEqual(cleanup(fixture.toString()), 'function fixture() {return true}')
   done()
 })
